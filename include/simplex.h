6909b7b3ba58cbcc201fb9dacf6cb091cae5b055
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2017-06-19 08:51:03 +0200

    First partial implementation

---
+++ include/simplex.h
@@ -0,0 +1,422 @@
+/*    This file is part of simplex
+      Copyright (C) 2017  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef _SIMPLEX_H_
+#define _SIMPLEX_H_
+
+#include <quicky_exception.h>
+#include <cstring>
+#include <type_traits>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <string>
+		
+namespace simplex
+{
+  typedef enum class equation_type
+    {
+      UNDEFINED = 0,
+      EQUATION,
+      INEQUATION_LT,
+      INEQUATION_GT
+    } t_equation_type;
+
+
+  template <typename COEF_TYPE>
+  class simplex
+  {
+  public:
+
+    /**
+       This constructor prepare a simplex in Canonical form
+       Max z = cx
+       a1 * x1 + a2 * x2 + ... + an * xn <= bn
+       xi >= 0 for all xi
+
+       Or
+
+       Min z = cx
+       a1 * x1 + a2 * x2 + ... + an * xn >= bn
+       xi >= 0 for all xi
+    */
+    inline simplex(unsigned int p_nb_variables,
+		   unsigned int p_nb_inequations_lt,
+		   unsigned int p_nb_equations,
+		   unsigned int p_nb_inequations_gt
+		   );
+
+    /**
+       Define coefficient for objective function
+       @param p_index : the value should be less than number of variables
+       @param value : value of coefficient in the formula Z = SUM(Cj * x)
+    */
+    inline void set_Z_coef(const unsigned int p_index,
+			   const COEF_TYPE & p_value
+			   );
+    /**
+       Define coefficient for B coefficients in A x = b
+       @param p_index : the value should be less than total number of equations
+       @param value : value of coefficient in b
+    */
+    inline void set_B_coef(const unsigned int p_index,
+			   const COEF_TYPE & p_value
+			   );
+
+    /**
+       Define coefficient for A coefficients in A x = b
+       @param p_equation_index : the value should be less than total number of equations
+       @param p_variable_index : the value should be less than number of variables
+       @param value : value of coefficient in A
+    */
+    inline void set_A_coef(const unsigned int p_equation_index,
+			   const unsigned int p_variable_index,
+			   const COEF_TYPE & p_value
+			   );
+
+    inline ~simplex(void);
+
+    /**
+       Display simplex array representation
+       @param p_stream stream where the display should be done
+       @return the modified stream
+     */
+    inline std::ostream & display_array(std::ostream & p_stream);
+
+    /**
+       Define equation type
+       @param Equation index
+       @param Equation type
+     */
+    inline void define_equation_type(const unsigned int & p_equation_index,
+				     const t_equation_type & p_equation_type
+				     );
+    /**
+       Method performing pivot to change the base
+       The A coefficient A[row,column] should be !0
+       @param p_row_index Row index
+       @param p_column_index Column index
+     */
+    inline void pivot(const unsigned int p_row_index,
+		      const unsigned int p_column_index
+		      );
+
+  private:
+    simplex(void) = delete;
+
+    /**
+       Method to set adjustement variable in the right place
+       @param index of inequation
+       @param value to set as coefficient
+    */
+    inline void set_adjustement_variable(const unsigned int & p_equation_index,
+					 const COEF_TYPE & p_value
+					 );
+    /**
+       Variable numbers without adjustment variables
+    */
+    unsigned int m_nb_variables;
+
+    /**
+       Number of inequations of type x1 + x2 + ... + xn <= value
+     */
+    unsigned int m_nb_inequations_lt;
+
+    /**
+       Number of equations of type x1 + x2 + ... + xn = value
+     */
+    unsigned int m_nb_equations;
+
+    /**
+       Number of inequations of type x1 + x2 + ... + xn >= value
+     */
+    unsigned int m_nb_inequations_gt;
+
+    /**
+       Variable counting the number of base variables specified
+     */
+    unsigned int m_nb_base_variables;
+
+    /**
+       Variable counting the number of adjustement variable specified
+     */
+    unsigned int m_nb_defined_adjustment_variables;
+
+    /**
+       Total number of variables including adjustment variables
+    */
+    unsigned int m_nb_all_variables;
+
+    /**
+       Total numer of equations after adjuments variables have been added
+    */
+    unsigned int m_nb_total_equations;
+
+    /**
+       Matrix A coefficients in Ax = b
+     */
+    COEF_TYPE * m_equation_coefs;
+
+    /**
+       b coefficients in Ax = b
+     */
+    COEF_TYPE * m_b_coefs;
+
+    /**
+       Coefficients of objective function in the form Z - SUM(Cj * X) = 0
+    */
+    COEF_TYPE * m_z_coefs;
+
+    /**
+       Z0 coefficient
+    */
+    COEF_TYPE m_z0;
+
+    /**
+       Equation types
+     */
+    t_equation_type * m_equation_types;
+  };
+
+  //----------------------------------------------------------------------------
+  std::ostream & operator<<(std::ostream & p_stream,
+			    const t_equation_type & p_equation_type
+			    )
+    {
+      switch(p_equation_type)
+	{
+	case t_equation_type::UNDEFINED:
+	  p_stream << "\"undef\"";
+	  break;
+	case t_equation_type::EQUATION:
+	  p_stream << "\"=\"";
+	  break;
+	case t_equation_type::INEQUATION_LT:
+	  p_stream << "\"<=\"";
+	break;
+	case t_equation_type::INEQUATION_GT:
+	  p_stream << "\">=\"";
+	break;
+	default:
+	  throw quicky_exception::quicky_logic_exception("Unknown equation_type value : "+ std::to_string((unsigned int)p_equation_type),__LINE__,__FILE__);
+	}
+      return p_stream;
+    }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  simplex<COEF_TYPE>::simplex(unsigned int p_nb_variables,
+			      unsigned int p_nb_inequations_lt,
+			      unsigned int p_nb_equations,
+			      unsigned int p_nb_inequations_gt
+		   ):
+    m_nb_variables(p_nb_variables),
+    m_nb_inequations_lt(p_nb_inequations_lt),
+    m_nb_equations(p_nb_equations),
+    m_nb_inequations_gt(p_nb_inequations_gt),
+    m_nb_base_variables(p_nb_inequations_lt + p_nb_inequations_gt),
+    m_nb_defined_adjustment_variables(0),
+    m_nb_all_variables(p_nb_variables + m_nb_base_variables),
+    m_nb_total_equations(p_nb_inequations_lt + p_nb_equations + p_nb_inequations_gt),
+    m_equation_coefs(new COEF_TYPE[m_nb_all_variables * m_nb_total_equations]),
+    m_b_coefs(new COEF_TYPE[m_nb_total_equations]),
+    m_z_coefs(new COEF_TYPE[m_nb_all_variables]),
+    m_z0(0),
+    m_equation_types(new t_equation_type[m_nb_total_equations])
+      {
+	static_assert(std::is_signed<COEF_TYPE>::value,"Simplex template parameter shoudl be signed");
+	memset(m_equation_coefs,0,m_nb_all_variables * m_nb_total_equations * sizeof(COEF_TYPE));
+	memset(m_b_coefs,0,m_nb_total_equations * sizeof(COEF_TYPE));
+	memset(m_z_coefs,0,m_nb_all_variables * sizeof(COEF_TYPE));
+	memset(m_equation_types, 0, m_nb_total_equations * sizeof(t_equation_type));
+      }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::set_Z_coef(const unsigned int p_index,
+				      const COEF_TYPE & p_value
+				      )
+    {
+      assert(m_z_coefs);
+      assert(p_index < m_nb_variables);
+      // Negate version of value due to the change of Z form between function call and storage variable
+      m_z_coefs[p_index] = - p_value;
+    }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::set_B_coef(const unsigned int p_index,
+				      const COEF_TYPE & p_value
+				      )
+    {
+      assert(m_b_coefs);
+      assert(p_index < m_nb_total_equations);
+      m_b_coefs[p_index] = p_value;
+    }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::set_A_coef(const unsigned int p_equation_index,
+				      const unsigned int p_variable_index,
+				      const COEF_TYPE & p_value
+				      )
+  {
+    assert(m_equation_coefs);
+    assert(p_equation_index < m_nb_total_equations);
+    assert(p_variable_index < m_nb_variables);
+    m_equation_coefs[p_equation_index * m_nb_all_variables + p_variable_index] = p_value;
+  }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::pivot(const unsigned int p_row_index,
+				 const unsigned int p_column_index
+				 )
+  {
+    assert(p_row_index < m_nb_total_equations);
+    assert(p_column_index < m_nb_all_variables);
+    COEF_TYPE l_pivot = m_equation_coefs[p_row_index * m_nb_all_variables + p_column_index ];
+    assert(l_pivot);
+
+    // Pivoting Z
+     COEF_TYPE l_q = m_z_coefs[p_column_index];
+     for(unsigned int l_index = 0;
+	 l_index < m_nb_all_variables;
+	 ++l_index
+	 )
+       {
+	 COEF_TYPE l_u = m_equation_coefs[p_row_index * m_nb_all_variables + l_index];
+	 m_z_coefs[l_index] = m_z_coefs[l_index] - (l_q * l_u) / l_pivot;
+       }
+     m_z0 = m_z0 - (l_q * m_b_coefs[p_row_index]) / l_pivot;
+
+    // Pivoting other rows
+    for(unsigned int l_row_index = 0;
+	l_row_index < m_nb_total_equations;
+	++l_row_index
+	)
+      {
+	if(l_row_index != p_row_index)
+	  {
+	    COEF_TYPE l_q = m_equation_coefs[l_row_index * m_nb_all_variables + p_column_index];
+	    m_b_coefs[l_row_index] = m_b_coefs[l_row_index] - (l_q * m_b_coefs[p_row_index]) / l_pivot;
+	    for(unsigned int l_index = 0;
+		l_index < m_nb_all_variables;
+		++l_index
+		)
+	      {
+		COEF_TYPE l_u = m_equation_coefs[p_row_index * m_nb_all_variables + l_index];
+		m_equation_coefs[l_row_index * m_nb_all_variables + l_index] = m_equation_coefs[l_row_index * m_nb_all_variables + l_index] - (l_q * l_u) / l_pivot;
+	      }
+	  }
+      }
+
+    // Particular case of pivot row
+    for(unsigned int l_index = 0;
+	l_index < m_nb_variables;
+	++l_index
+	)
+      {
+	m_equation_coefs[p_row_index * m_nb_all_variables + l_index] = m_equation_coefs[p_row_index * m_nb_all_variables + l_index] / l_pivot;
+      }
+    m_b_coefs[p_row_index] = m_b_coefs[p_row_index] / l_pivot;
+  }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  std::ostream & simplex<COEF_TYPE>::display_array(std::ostream & p_stream)
+    {
+      p_stream << "Z\t";
+     for(unsigned int l_index = 0;
+	 l_index < m_nb_all_variables;
+	 ++l_index
+	 )
+       {
+	 p_stream << m_z_coefs[l_index] << "\t";
+       }
+     p_stream << "|\t" << m_z0 << std::endl;
+     for(unsigned int l_row_index = 0;
+	 l_row_index < m_nb_total_equations;
+	 ++l_row_index
+	 )
+       {
+	 p_stream << " \t";
+	 for(unsigned int l_index = 0;
+	     l_index < m_nb_all_variables;
+	     ++l_index
+	     )
+	   {
+	     p_stream << m_equation_coefs[l_row_index * m_nb_all_variables + l_index] << "\t";
+	   }
+	 p_stream << "|\t" << m_b_coefs[l_row_index] << std::endl;
+      }
+     return p_stream;
+    }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::set_adjustement_variable(const unsigned int & p_equation_index,
+						    const COEF_TYPE & p_value
+						    )
+    {
+      unsigned int l_column_index = m_nb_variables + m_nb_defined_adjustment_variables;
+      m_equation_coefs[p_equation_index * m_nb_all_variables + l_column_index] = p_value;
+      ++m_nb_defined_adjustment_variables;
+    }
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  void simplex<COEF_TYPE>::define_equation_type(const unsigned int & p_equation_index,
+						const t_equation_type & p_equation_type
+						)
+    {
+      assert(p_equation_index < m_nb_total_equations);
+      m_equation_types[p_equation_index] = p_equation_type;
+      switch(p_equation_type)
+	{
+	case t_equation_type::UNDEFINED:
+	  throw quicky_exception::quicky_logic_exception("Try to set undefined equation type for equation" + std::to_string(p_equation_index),__LINE__,__FILE__);
+	  break;
+	case t_equation_type::EQUATION:
+	  // Nothing to do
+	  break;
+	case t_equation_type::INEQUATION_LT:
+	  set_adjustement_variable(p_equation_index,1);
+	  break;
+	case t_equation_type::INEQUATION_GT:
+	  set_adjustement_variable(p_equation_index,-1);
+	  break;
+	default:
+	  throw quicky_exception::quicky_logic_exception("Unknown equation_type value : "+ std::to_string((unsigned int)p_equation_type),__LINE__,__FILE__);
+	}
+    }
+
+  //----------------------------------------------------------------------------
+  template <typename COEF_TYPE>
+  simplex<COEF_TYPE>::~simplex(void)
+    {
+      delete[] m_equation_types;
+      m_equation_types = nullptr;
+      delete[] m_z_coefs;
+      m_z_coefs = nullptr;
+      delete[] m_b_coefs;
+      m_b_coefs = nullptr;
+      delete[] m_equation_coefs;
+      m_equation_coefs = nullptr;
+    }
+}
+
+#endif // _SIMPLEX_H_
+// EOF
--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,134 @@
       along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 #include "quicky_exception.h"
+#include "simplex.h"
 #include <iostream>
+
+
+void test_case1(void)
+{
+  // Example
+  // Max Z -1 * X1 - 4 * X2 - 3 * X3           = 0
+  //        2 * X1 + 2 * X2 + 1 * X3 + X4      = 4
+  //            X1 + 2 * X2 + 2 * X3 +    + X5 = 6
+  simplex::simplex<double> l_simplex(5, // Number of variables : x1 and x2
+				  0, // Number of inequations with the form A x <= b
+				  2, // Number of equations with the form A x = b
+				  0  // Number of inequations with the form A x >= b
+				  );
+  l_simplex.set_Z_coef(0,1);
+  l_simplex.set_Z_coef(1,4);
+  l_simplex.set_Z_coef(2,3);
+  l_simplex.set_B_coef(0,4);
+  l_simplex.set_B_coef(1,6);
+  l_simplex.set_A_coef(0,0,2);
+  l_simplex.set_A_coef(0,1,2);
+  l_simplex.set_A_coef(0,2,1);
+  l_simplex.set_A_coef(0,3,1);
+  l_simplex.set_A_coef(1,0,1);
+  l_simplex.set_A_coef(1,1,2);
+  l_simplex.set_A_coef(1,2,2);
+  l_simplex.set_A_coef(1,4,1);
+  l_simplex.define_equation_type(0,simplex::t_equation_type::EQUATION);
+  l_simplex.define_equation_type(1,simplex::t_equation_type::EQUATION);
+
+  std::cout << "---------------------------------" << std::endl;
+  l_simplex.display_array(std::cout);
+
+  l_simplex.pivot(0,0);
+
+  std::cout << "---------------------------------" << std::endl;
+  l_simplex.display_array(std::cout);
+
+  l_simplex.pivot(0,1);
+
+  std::cout << "---------------------------------" << std::endl;
+  l_simplex.display_array(std::cout);
+
+  l_simplex.pivot(1,2);
+
+  std::cout << "---------------------------------" << std::endl;
+  l_simplex.display_array(std::cout);
+}
 
 //------------------------------------------------------------------------------
 int main(int argc,char ** argv)
 {
   try
     {
-    }
+
+      test_case1();
+      // Example
+      // Max z = 1000 x1 + 1200 x2
+      // 10 x1 + 5 x2 <= 200
+      // 2 x1 + 3 x2 <= 60
+      // x1 <= 34
+      // x2 <= 14
+      // x1 x2 >= 0
+
+      unsigned int l_max = 0;
+      for(unsigned int l_x1 = 0;
+	  l_x1 <= 34;
+	  ++l_x1
+	  )
+	{
+	  for(unsigned int l_x2 = 0;
+	      l_x2 < 14;
+	      ++l_x2
+	      )
+	    {
+	      if(2 * l_x1 + 3 * l_x2 <= 60 && 10 * l_x1 + 5 * l_x2 <=200)
+		{
+		  unsigned int l_result = 1000 * l_x1 + 1200 * l_x2;
+		  if(l_result > l_max)
+		    {
+		      l_max = l_result;
+		      std::cout << "(" << l_x1 << "," << l_x2 << ") = " << l_max << std::endl;
+		    }
+		}
+	    }
+	}
+
+      simplex::simplex<double> l_simplex(2, // Number of variables : x1 and x2
+				      4, // Number of inequations with the form A x <= b
+				      0, // Number of equations with the form A x = b
+				      0  // Number of inequations with the form A x >= b
+				      );
+      l_simplex.set_Z_coef(0,1000);
+      l_simplex.set_Z_coef(1,1200);
+      l_simplex.set_B_coef(0,200);
+      l_simplex.set_B_coef(1,60);
+      l_simplex.set_B_coef(2,34);
+      l_simplex.set_B_coef(3,14);
+      l_simplex.set_A_coef(0,0,10);
+      l_simplex.set_A_coef(0,1,5);
+      l_simplex.set_A_coef(1,0,2);
+      l_simplex.set_A_coef(1,1,3);
+      l_simplex.set_A_coef(2,0,1);
+      l_simplex.set_A_coef(3,1,1);
+      l_simplex.define_equation_type(0,simplex::t_equation_type::INEQUATION_LT);
+      l_simplex.define_equation_type(1,simplex::t_equation_type::INEQUATION_LT);
+      l_simplex.define_equation_type(2,simplex::t_equation_type::INEQUATION_LT);
+      l_simplex.define_equation_type(3,simplex::t_equation_type::INEQUATION_LT);
+
+      std::cout << "---------------------------------" << std::endl;
+      l_simplex.display_array(std::cout);
+
+      l_simplex.pivot(0,0);
+
+      std::cout << "---------------------------------" << std::endl;
+      l_simplex.display_array(std::cout);
+
+      l_simplex.pivot(1,1);
+
+      std::cout << "---------------------------------" << std::endl;
+      l_simplex.display_array(std::cout);
+
+      l_simplex.pivot(0,2);
+
+      std::cout << "---------------------------------" << std::endl;
+      l_simplex.display_array(std::cout);
+   }
   catch(quicky_exception::quicky_runtime_exception & e)
     {
       std::cout << "ERROR : " << e.what() << std::endl ;
